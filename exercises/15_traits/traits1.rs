be93e405fd192a8c7fa17715f45772d2c3d9b9cb
FHaggs <pedrofamh@protonmail.com>
2025-06-03 14:54:43 -0300

    Till quiz 3

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -1,3 +1,4 @@
+
 // The trait `AppendBar` has only one function which appends "Bar" to any object
 // implementing this trait.
 trait AppendBar {
@@ -6,6 +7,9 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        format!("{}Bar", self)
+    }
 }
 
 fn main() {
